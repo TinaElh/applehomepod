1b550c1ca8c6d91e66421912eb68bfca44f31a8a
Tina <tinaelha@gmail.com>
2019-02-20 16:19:02 +0100

    div light homekit changements

--- scripts/homekit.js
+++ scripts/homekit.js
@@ -2,12 +2,13 @@ const light = document.querySelector('.buttonLight')
 const tv = document.querySelector('.buttonTV')
 const window = document.querySelector ('.buttonWindow')
 const picture = document.querySelector ('.home')
+const black = document.querySelector ('.black')
 
 
 buttonLight.addEventListener(
     'click', 
     function()
     {
-        home.style.opacity="1px"
+        black.style.opacity="5px"
     }
 )
